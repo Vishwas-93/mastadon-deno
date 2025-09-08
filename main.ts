d11573556e5a6763919e6a312a7f0effde021367
Vishwas-93 <vishwas.vasudevamanja@gmail.com>
2025-09-07 22:44:32 -0500

    Simple server

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 export function add(a: number, b: number): number {
-  return a + b;
+  return a + b
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  Deno.serve({ port: 4242, hostname: '0.0.0.0' }, (req) => {
+    console.log(req)
+    if (req.body) {
+      const body = req.json()
+      console.log('Body:', body)
+    }
+    return new Response('Hello, World!')
+  })
+  // console.log('Add 2 + 3 =', add(2, 3))
 }
